4a4d696e3bb58ec382f3f757bfc7daabd24b7bd4
zackari sl. <zackaria.celimane@gmail.com>
2024-08-13 17:41:58 +0100

    fixing security alert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import { FaSquareGithub, FaSquareXTwitter } from "react-icons/fa6";
+import { FaSquareGithub, FaSquareXTwitter } from 'react-icons/fa6';
 import React, { useState } from 'react';
-import sun from './theme-icon/sun.png'
-import moon from './theme-icon/moon.png'
+import sun from './theme-icon/sun.png';
+import moon from './theme-icon/moon.png';
 
 const Header = () => {
 	const [isDarkMode, setIsDarkMode] = useState(false);
@@ -11,19 +11,26 @@ const Header = () => {
 	};
 
 	const themeClass = isDarkMode ? 'dark-theme' : 'light-theme';
-
 	document.body.className = themeClass;
+
 	return (
 		<header className='my-4 py-2 shadow-md'>
 			<div className='flex gap-4 justify-between mx-10'>
-				<h1 className={`text-center text-2xl font-bold ${themeClass === 'light-theme' ? 'text-gray-800' : 'text-white'}`}>
+				<h1
+					className={`text-center text-2xl font-bold ${
+						themeClass === 'light-theme' ? 'text-gray-800' : 'text-white'
+					}`}>
 					unFetch
 				</h1>
 				<nav>
 					<ul className='flex gap-4 text-2xl'>
 						<li>
-							<button onClick={toggleTheme} >
-								{isDarkMode ? <img src={sun} alt="Sun Icon" className="h-6" /> : <img src={moon} alt="Moon Icon" className="h-6" />}
+							<button onClick={toggleTheme}>
+								{isDarkMode ? (
+									<img src={sun} alt='Sun Icon' className='h-6' />
+								) : (
+									<img src={moon} alt='Moon Icon' className='h-6' />
+								)}
 							</button>
 						</li>
 						<li>
